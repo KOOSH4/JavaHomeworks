f2e5a4160cddbe38841d356724c7e55f5aa04637
koosh4 <koosha.owlad@gmail.com>
2022-10-25 23:38:29 +0200

    Problem Solved

---
+++ W1/T2/RandomSeq.java
@@ -0,0 +1,25 @@
+/**
+* Advanced Object Oriented Programming with Java, WS 2022
+* Problem: Random Seq
+* Link: https://open.kattis.com/problems/carrots
+* @author Koosha
+* @version 1.0, 10/25/2022
+*
+* Status : Accepted
+* Runtime: 0.102
+*/
+
+package W1.T2;
+
+import java.util.Random;
+
+public class RandomSeq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rand = new Random();
+		double result = rand.nextDouble(1);
+		System.out.println(result);
+	}
+
+}
