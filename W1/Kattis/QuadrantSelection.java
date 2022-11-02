068cc15cbdc5a36467339a5a1116ec82c5e5969a
koosh4 <koosha.owlad@gmail.com>
2022-11-02 12:17:52 +0100

    Commented some lines out

--- W1/Kattis/QuadrantSelection.java
+++ W1/Kattis/QuadrantSelection.java
@@ -7,8 +7,8 @@ public class QuadrantSelection{
         Scanner input = new Scanner(System.in);
 //        System.out.println("enter X: ");
         int xInput = input.nextInt();
-        System.out.println("enter Y: ");
-//        int yInput = input.nextInt();
+//        System.out.println("enter Y: ");
+        int yInput = input.nextInt();
         if( xInput > 0 ){
             if (yInput > 0){
                 System.out.println(1);
