6723ba34cfdf68568deb84c81e6c0994b97e4599
koosh4 <koosha.owlad@gmail.com>
2022-11-07 13:38:43 +0100

    Challenge Solved

---
+++ W2/T1/Point.java
@@ -0,0 +1,12 @@
+package JavaHomeworks.W2.T1;
+
+public class Point {
+    public int x = 0;
+    public int y = 0;
+
+    // a constructor!
+    public Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+}
